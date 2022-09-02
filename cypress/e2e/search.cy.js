794dd7242a58ab3c9b5dbafacff84b7b90f2e0aa
Jennifer Rondón <rondon.jenni@gmail.com>
2022-09-02 00:14:55 -0400

    Solved T1 and T2

---
+++ cypress/e2e/search.cy.js
@@ -0,0 +1,29 @@
+
+describe('Search elements', () => {
+
+    beforeEach(() =>{
+        cy.visit('/');
+    });
+    
+    it('search for element wih multiple results', () => {
+        cy.fixture('index').then((index) => {
+            cy.get(index.searchBox).type('dress');
+            cy.get(index.searchButton).click();
+        });
+        cy.fixture('searchResult').then((searchResult) => {  
+            cy.get(searchResult.title).should('contain', "dress")
+        })
+    });
+
+    it('Search for element with no results', () =>{
+        cy.fixture('index').then((index) => {
+            cy.get(index.searchBox).type('qwerty');
+            cy.get(index.searchButton).click();
+        });
+
+        cy.fixture('searchResult').then((searchResult) => {  
+            cy.get(searchResult.alert).should('contain', 'No results were found for your search')
+        })
+
+    })
+})
